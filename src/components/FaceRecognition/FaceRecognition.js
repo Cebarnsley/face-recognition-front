8dd76d924ed8e50434a6bb121d0d3e5dd693c2a5
Candice Barnsley <cebarnsley@gmail.com>
2018-11-22 14:36:29 +0000

    changes to facerecognition file

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'; 
 import './FaceRecognition.css'; 
 
@@ -15,22 +14,4 @@ const FaceRecognition = ({imageUrl, box}) => {
 	);
 }
 
-=======
-import React from 'react'; 
-import './FaceRecognition.css'; 
-
-
-
-const FaceRecognition = ({imageUrl, box}) => {
-	return (
-		<div className='center ma'>
-		<div className='absolute mt2'>
-			<img id='inputimage' alt='' src={imageUrl} width='auto' height='300px'/>
-			<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-			</div>
-		</div>
-	);
-}
-
->>>>>>> bf809638a078d497fe8298c5e5ed0ef6a1204ca9
 export default FaceRecognition;
