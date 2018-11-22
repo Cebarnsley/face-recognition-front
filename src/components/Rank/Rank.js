61459e921c3ba42982bb052c2b2ee8cd98ab2225
Candice Barnsley <cebarnsley@gmail.com>
2018-11-22 14:33:34 +0000

    changes to rank file

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'; 
 
 
@@ -16,23 +15,4 @@ const Rank = ({name, entries}) => {
 	);
 }
 
-=======
-import React from 'react'; 
-
-
-
-const Rank = ({name, entries}) => {
-	return (
-		<div>
-			<div className=' white f3'>
-				{`${name}, your current entry count is....`}
-			</div>
-			<div className=' white f1'>
-				{entries}
-			</div>
-		</div>
-	);
-}
-
->>>>>>> bf809638a078d497fe8298c5e5ed0ef6a1204ca9
 export default Rank;
